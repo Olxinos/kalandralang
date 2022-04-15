ff1732b553ea0ff923d9dc8c690f1ef6672d292d
olxinos <olxinos.etenn@orange.fr>
2022-04-15 06:30:39 +0200

    uses current mod values instead of min mod values for pdps calculation

--- src/mod.ml
+++ src/mod.ml
@@ -59,7 +59,7 @@ let roll_modifier modifier : t =
 
 let get_stat_value ?(acc = 0) (modifier : t) (stat_id : Id.t) : int =
   let accumulate acc (stat : stat) =
-    if compare stat.id stat_id == 0 then acc + stat.min else acc
+    if compare stat.id stat_id == 0 then acc + stat.current else acc
   in
     List.fold_left accumulate acc modifier.stats
 
